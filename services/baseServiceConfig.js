59e47f0292786b1411effeff771e1b9f1a291309
Horopter <santoshdesai12@hotmail.com>
2025-03-03 14:56:49 +0530

    Changed backend to new one

--- services/baseServiceConfig.js
+++ services/baseServiceConfig.js
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = "https://tattvachandrika-magazine-119814d66a82.herokuapp.com";
-export const API_BASE_URL = "https://tattvachandrika-backend-a5f5d107dcde.herokuapp.com/";
+export const API_BASE_URL = "https://tattvachandrika-backend-a5f5d107dcde.herokuapp.com";
 export default API_BASE_URL;

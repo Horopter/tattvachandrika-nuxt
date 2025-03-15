a51b90479231eb1c323f269132d433821dcb8ca8
Horopter <santoshdesai12@hotmail.com>
2025-03-15 13:21:03 +0530

    Addressing login issues

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  runtimeConfig: {
+    public: {
+      API_ENV: process.env.NUXT_PUBLIC_API_ENV || 'localhost'
+    }
+  },
+  vite: {
+    envPrefix: ['NUXT_', 'VITE_'] // This is usually the default
+  },
   app: {
     baseURL: process.env.BASE_URL || '/', // default is root
   },
--- services/baseServiceConfig.js
+++ services/baseServiceConfig.js
@@ -1,16 +1,19 @@
-// Define your API base URLs for each environment
+// baseconfig.js
+
 const apiUrls = {
     herokuOld: "https://tattvachandrika-magazine-119814d66a82.herokuapp.com",
     heroku: "https://tattvachandrika-backend-a5f5d107dcde.herokuapp.com",
-    website: "http://tattvachandrika.com",
+    website: "https://tattvachandrika.com",
     localhost: "http://localhost:3000"
   };
 
-  console.log("Horopter:: API_ENV:", process.env, process.env.API_ENV);
+  console.log("Full env:", import.meta.env);
+  console.log("NUXT_PUBLIC_API_ENV:", import.meta.env.NUXT_PUBLIC_API_ENV);
+  console.log("Horopter:: API_ENV:", import.meta.env.NUXT_PUBLIC_API_ENV);
   
-  // Determine the environment from the API_ENV variable (default to 'localhost')
-  const env = process.env.API_ENV || "localhost";
+  // Determine the environment from the NUXT_PUBLIC_API_ENV variable (default to 'localhost')
+  const env = import.meta.env.NUXT_PUBLIC_API_ENV || "localhost";
   
   // Set the API_BASE_URL based on the environment key; if the key isn't found, fallback to localhost
   export const API_BASE_URL = apiUrls[env] || apiUrls.localhost;
-  export default API_BASE_URL;
+  export default API_BASE_URL;  

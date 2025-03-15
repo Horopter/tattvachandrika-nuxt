a4447d38f9c6f2ebe0f78e8389ac80aa88754c32
Horopter <santoshdesai12@hotmail.com>
2025-03-15 05:50:26 +0530

    baseURL flexibility introduced

--- services/baseServiceConfig.js
+++ services/baseServiceConfig.js
@@ -1,3 +1,14 @@
-// export const API_BASE_URL = "https://tattvachandrika-magazine-119814d66a82.herokuapp.com";
-export const API_BASE_URL = "https://tattvachandrika-backend-a5f5d107dcde.herokuapp.com";
-export default API_BASE_URL;
+// Define your API base URLs for each environment
+const apiUrls = {
+    herokuOld: "https://tattvachandrika-magazine-119814d66a82.herokuapp.com",
+    heroku: "https://tattvachandrika-backend-a5f5d107dcde.herokuapp.com",
+    website: "http://tattvachandrika.com",
+    localhost: "http://localhost:3000"
+  };
+  
+  // Determine the environment from the API_ENV variable (default to 'localhost')
+  const env = process.env.API_ENV || "localhost";
+  
+  // Set the API_BASE_URL based on the environment key; if the key isn't found, fallback to localhost
+  export const API_BASE_URL = apiUrls[env] || apiUrls.localhost;
+  export default API_BASE_URL;
